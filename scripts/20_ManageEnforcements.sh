25520a8cd70924ca9be9ac3462b281a4bc247a9d
staillansag <103417302+staillansag@users.noreply.github.com>
2023-08-13 09:59:10 +0200

    .

--- scripts/20_ManageEnforcements.sh
+++ scripts/20_ManageEnforcements.sh
@@ -17,46 +17,60 @@ cp ./templates/enforcements.json ./tmp_enforcements.json
 # We then create the individual policy enforcement according the the specifications found in the manifest.json file
 # Each time we create a policy enforcement, we enrich the enforcements template with its ID
 
+echo "Transport policy enforcement"
 array_length=$(jq '.transport | length' manifest.json)
 if [ "$array_length" -gt 0 ]; then
     createPolicyEnforcement "./templates/transport.jq" "manifest.json" "transport" "./tmp_enforcements.json"
 fi
 
+echo "IAM policy enforcement"
 if jq -e 'has("iam")' manifest.json > /dev/null; then
     createPolicyEnforcement "./templates/iam.jq" "manifest.json" "IAM" "./tmp_enforcements.json"
 fi
 
+echo "Request Transformation policy enforcement"
 if jq -e 'has("requestTransformations")' manifest.json > /dev/null; then
     createPolicyEnforcement "./templates/reqt.jq" "manifest.json" "requestPayloadProcessing" "./tmp_enforcements.json"
 fi
 
+echo "Request Validation policy enforcement"
 if jq -e 'has("requestValidation")' manifest.json > /dev/null; then
     createPolicyEnforcement "./templates/reqv.jq" "manifest.json" "requestPayloadProcessing" "./tmp_enforcements.json"
 fi
 
+echo "Routing policy enforcement"
 if jq -e 'has("routing")' manifest.json > /dev/null; then
     createPolicyEnforcement "./templates/strouting.jq" "manifest.json" "routing" "./tmp_enforcements.json"
 fi
 
+echo "Backend Authentication policy enforcement"
 if jq -e 'has("backendAuthentication")' manifest.json > /dev/null; then
     createPolicyEnforcement "./templates/beauth.jq" "manifest.json" "routing" "./tmp_enforcements.json"
 fi
 
+echo "Throttling policy enforcement"
 if jq -e 'has("throttling")' manifest.json > /dev/null; then
     createPolicyEnforcement "./templates/throttling.jq" "manifest.json" "LMT" "./tmp_enforcements.json"
 fi
 
+echo "Caching policy enforcement"
 if jq -e 'has("caching")' manifest.json > /dev/null; then
     createPolicyEnforcement "./templates/caching.jq" "manifest.json" "LMT" "./tmp_enforcements.json"
 fi
 
+echo "Response Transformation policy enforcement"
 if jq -e 'has("responseTransformations")' manifest.json > /dev/null; then
     createPolicyEnforcement "./templates/rest.jq" "manifest.json" "responseProcessing" "./tmp_enforcements.json"
 fi
 
+echo "Response Validation policy enforcement"
 if jq -e 'has("responseValidation")' manifest.json > /dev/null; then
     createPolicyEnforcement "./templates/resv.jq" "manifest.json" "responseProcessing" "./tmp_enforcements.json"
 fi
+
+echo
+cat ./tmp_enforcements.json
+echo
 
 # Then we update the default policy with the content of the enforcements template
 
