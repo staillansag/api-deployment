ac5f67a45c522aa552ceff55e26d5c8552090259
staillansag <103417302+staillansag@users.noreply.github.com>
2023-08-13 09:00:41 +0200

    .

--- scripts/11_CreateAPIDefinition.sh
+++ scripts/11_CreateAPIDefinition.sh
@@ -1,5 +1,13 @@
 . ./scripts/00_Utils.sh
 
+echo "APIGW_URL         = ${APIGW_URL}"
+echo "APIGW_USERNAME    = ${APIGW_USERNAME}"
+echo "APIGW_PASSWORD    = ${APIGW_PASSWORD}"
+echo "SPEC_TYPE         = ${SPEC_TYPE}"
+echo "API_SPEC_FILE     = ${API_SPEC_FILE}"
+echo "API_NAME          = ${API_NAME}"
+echo "API_VERSION       = ${API_VERSION}"
+
 RESPONSE=$(curl -s --location --request POST "${APIGW_URL}/apis" \
 -u ${APIGW_USERNAME}:${APIGW_PASSWORD} \
 --header 'accept: application/json' \
@@ -20,9 +28,10 @@ else
     echo "##vso[task.setvariable variable=API_ID;]${API_ID}"
 
     # We also store the ID of the default policy attached to this API
-    POLICY_ID=$(echo $RESPONSE | jq -r '.apiResponse.api.policies[0]')
-    echo "##vso[task.setvariable variable=POLICY_ID;]${POLICY_ID}"
+    API_POLICY_ID=$(echo $RESPONSE | jq -r '.apiResponse.api.policies[0]')
+    echo "##vso[task.setvariable variable=POLICY_ID;]${API_POLICY_ID}"
 fi
+
 
 # json=$(jq -f ./templates/searchteam.jq manifest.json)
 
