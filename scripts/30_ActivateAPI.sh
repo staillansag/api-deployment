7069de8e5123318138dd8aebee58136ebb405910
staillansag <103417302+staillansag@users.noreply.github.com>
2023-08-13 12:47:18 +0200

    .

--- scripts/30_ActivateAPI.sh
+++ scripts/30_ActivateAPI.sh
@@ -4,7 +4,7 @@ echo "APIGW_URL         = ${APIGW_URL}"
 echo "APIGW_USERNAME    = ${APIGW_USERNAME}"
 echo "API_ID            = ${API_ID}"
 
-RESPONSE=$(curl -s --location --request PUT "${APIGW_URL}/apis${API_ID}/activate" \
+RESPONSE=$(curl -s --location --request PUT "${APIGW_URL}/apis/${API_ID}/activate" \
 -u ${APIGW_USERNAME}:${APIGW_PASSWORD} \
 --header 'accept: application/json')
 
@@ -12,5 +12,6 @@ ERROR_DETAILS=$(echo $RESPONSE | jq -r '.errorDetails')
 
 if [ "$ERROR_DETAILS" != "null" ] ; then
     echo "--- API activation failed: $ERROR_DETAILS"
+    echo ${RESPONSE}
     exit 1
 fi
